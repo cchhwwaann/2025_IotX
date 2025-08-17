5fd675ae98e3ced0f85a9ed9c2a8f3177c977827
cchhwwaann <https://github.com/cchhwwaann/Sus_motion_Control.git>
2025-08-17 13:41:28 +0900

    8.17

--- hand_detector.py
+++ hand_detector.py
@@ -10,16 +10,17 @@ def start_hand_detection():
     손 인식을 시작하고 Flask 서버로 zone 값을 전송하는 함수
     """
     mp_hands = mp.solutions.hands
-    hands = mp_hands.Hands(max_num_hands=2) # 두 손까지 인식하도록 설정
+    hands = mp_hands.Hands(max_num_hands=1)
     cap = cv2.VideoCapture(0)
     screen_width, _ = pyautogui.size()
     
-    div_zone1_end = screen_width // 3
-    div_zone2_end = (screen_width // 3) * 2
+    # 각 구역의 경계 설정
+    zone1_end = screen_width // 3
+    zone2_end = (screen_width // 3) * 2
     
     server_url = "http://127.0.0.1:5000/command"
-    prev_zone = -1
-
+    active_zone = 0
+    
     while cap.isOpened():
         success, image = cap.read()
         if not success:
@@ -29,50 +30,31 @@ def start_hand_detection():
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = hands.process(image_rgb)
         
-        zone = 0
+        current_zone = 0
         
-        # ###################### 수정된 로직: 가장 큰 손 선택 ######################
         if results.multi_hand_landmarks:
-            largest_hand_landmarks = None
-            max_hand_area = 0
-
-            # 감지된 모든 손을 순회하며 가장 큰 손 찾기
-            for hand_landmarks in results.multi_hand_landmarks:
-                # 손 랜드마크의 x, y 좌표 범위를 계산하여 손의 크기(면적) 추정
-                x_coords = [landmark.x for landmark in hand_landmarks.landmark]
-                y_coords = [landmark.y for landmark in hand_landmarks.landmark]
-                
-                hand_width = max(x_coords) - min(x_coords)
-                hand_height = max(y_coords) - min(y_coords)
-                hand_area = hand_width * hand_height
-
-                if hand_area > max_hand_area:
-                    max_hand_area = hand_area
-                    largest_hand_landmarks = hand_landmarks
+            hand_landmarks = results.multi_hand_landmarks[0]
             
-            # 가장 큰 손의 좌표를 사용해 zone 계산
-            if largest_hand_landmarks:
-                index_mcp = largest_hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP]
-                pinky_mcp = largest_hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP]
-                palm_center_x = (index_mcp.x + pinky_mcp.x) / 2
-                x_screen = int(palm_center_x * screen_width)
-                
-                if x_screen < div_zone1_end:
-                    zone = 1
-                elif x_screen < div_zone2_end:
-                    zone = 2
-                else:
-                    zone = 3
-        # ##########################################################################
+            # 검지 손가락 끝(랜드마크 8번)의 좌표 가져오기
+            index_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+            
+            current_x = int(index_finger_tip.x * screen_width)
+            
+            # 손의 위치에 따라 zone을 결정
+            if current_x < zone1_end:
+                current_zone = 1
+            elif current_x < zone2_end:
+                current_zone = 2
+            else:
+                current_zone = 3
         
-        # zone 값이 이전과 달라졌을 때만 서버에 POST 요청
-        if zone != prev_zone:
+        if current_zone != active_zone:
             try:
-                data = {"zone": zone}
+                data = {"zone": current_zone}
                 requests.post(server_url, json=data)
             except requests.exceptions.ConnectionError:
                 pass
-            prev_zone = zone
+            active_zone = current_zone
             
         time.sleep(0.01)
 
